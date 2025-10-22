0a3d51cf665d6721e088bff2f0d332e0004d10bd
panwei <pw@hosecloud.com>
2025-10-22 10:13:50 +0800

    Initial commit: 老潘的跑步记录

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,38 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const getBasePath = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return baseUrl === '/' ? '' : baseUrl;
+};
+
+const data: ISiteMetadataResult = {
+  siteTitle: '老潘的跑步记录',
+  siteUrl: 'https://weipanner.github.io/running_page',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: '老潘的个人跑步数据可视化',
+  navLinks: [
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    },
+    {
+      name: 'Blog',
+      url: 'https://github.com/weipanner',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+    },
+  ],
+};
+
+export default data;
